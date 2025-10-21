99223552fa8484790de4cb21e63ba8c8ffcdf2fd
Manikanta Gangisetti <mgangisetti@wellabe.com>
2025-10-21 12:42:53 -0500

    Add ESLint configuration for specific directories
    Configure ESLint to run only on 'pages' and 'utils' directories during production builds.

--- next.config.js
+++ next.config.js
@@ -9,4 +9,8 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+}
